1b4a388d1e99b6b911ec69284d898b5c8e0033ed
AebadUQ <aebadulquadir123@gmail.com>
2022-10-17 22:54:34 -0700

    committwo

---
+++ Slider Custom/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      
+     <div class="overlay">
+  <div><span className='para'>Mera naam Aebad ul quadir hai </span></div>
+</div>
+
+  );
+}
+
+export default App;
